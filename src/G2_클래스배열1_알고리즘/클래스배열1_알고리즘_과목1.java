3f2201ffa336065868a6df81b50419c761e2f464
janghyeon-u <janghyeon-u@localhost>
2022-11-06 02:27:46 +0900

    java_G

--- src/G2_클래스배열1_알고리즘/클래스배열1_알고리즘_과목1.java
+++ src/G2_클래스배열1_알고리즘/클래스배열1_알고리즘_과목1.java
@@ -31,25 +31,29 @@ public class 클래스배열1_알고리즘_과목1 {
 		//문제1) 위 배열을 클래스 배열에 저장 
 				
 		int stSize = numberList.length;
-		Student [] stList = new Student[stSize];
-		for(int i = 0; i < stSize; i++) 
+		Student[] stList = new Student[stSize];
+		
+		for(int i = 0; i < stSize; i++)
 		{
 			stList[i] = new Student();
-			stList[i].name = nameList[i];
 			stList[i].number = numberList[i];
+			stList[i].name = nameList[i];
 		}
 		
 		int subSize = studentNumberList.length;
 		Subject[] subList = new Subject[subSize];
-		for(int i =0; i < subSize; i++) 
+		
+		for(int i = 0; i < subSize; i++)
 		{
 			subList[i] = new Subject();
 			subList[i].studentNumber = studentNumberList[i];
 			subList[i].subject = subjectList[i];
 			subList[i].score = scoreList[i];
 			subList[i].rank = rankList[i];
-									
-		}		
+			
+			
+		}
+		
 		
 		while(true) 
 		{
@@ -61,23 +65,25 @@ public class 클래스배열1_알고리즘_과목1 {
 			System.out.println("[5] 과목별 랭킹 1등의 이름 과목 점수 출력 ");
 			
 			int sel = scan.nextInt();
+			
 			if(sel == 0) 
 			{
 				break;
 			}
 			
-			else if(sel == 1) 
-			{			
-				for(int i = 0; i < stSize; i++) 
+			else if(sel == 1)
+			{
+				for(int i = 0; i < stSize; i++)
 				{
 					System.out.println(stList[i].number + " " + stList[i].name);
 				}
+				
 				System.out.println("---------------------------------------");	
 			}
 			
 			else if(sel == 2)
 			{
-				for(int i =0; i < subSize; i++) 
+				for(int i = 0; i < subSize; i++)
 				{
 					System.out.print(subList[i].studentNumber + " ");
 					System.out.print(subList[i].subject + " ");
@@ -85,35 +91,36 @@ public class 클래스배열1_알고리즘_과목1 {
 					System.out.print(subList[i].rank + " ");
 					System.out.println();
 				}
-				System.out.println("---------------------------------------");	
+				
+				System.out.println("---------------------------------------");
 			}
 			
-			else if(sel == 3) 
+			else if(sel == 3)
 			{
-				for(int i = 0; i <subSize; i++ ) 
+				for(int i = 0; i < subSize; i++)
 				{
 					int rank = 1;
-					for(int j = 0 ; j < subSize; j++) 
+					
+					for(int j = 0; j < subSize; j++)
 					{
-						if(subList[i].subject.equals(subList[j].subject)) 
+						if(subList[i].subject.equals(subList[j].subject))
 						{
-							if(subList[i].score < subList[j].score) 
+							if(subList[i].score < subList[j].score)
 							{
 								rank += 1;
-							}
+							}	
 						}
 					}
 					
-					subList[i].rank = rank;		
+					subList[i].rank = rank;
 				}
-				
 			}
 			
-			else if(sel == 4) 
+			else if(sel == 4)
 			{
-				for(int i = 0; i < stSize; i++) 
+				for(int i = 0; i < stSize; i++)
 				{
-					for(int j =0; j < subSize; j++) 
+					for(int j = 0; j < subSize; j++)
 					{
 						if(stList[i].number == subList[j].studentNumber)
 						{
@@ -128,32 +135,12 @@ public class 클래스배열1_알고리즘_과목1 {
 				}
 			}
 			
-			else if(sel == 5) 
+			else if(sel == 5)
 			{
-				for(int i = 0; i <subSize; i++ ) 
-				{
-					int rank = 1;
-					for(int j = 0 ; j < subSize; j++) 
-					{
-						if(subList[i].subject.equals(subList[j].subject)) 
-						{
-							if(subList[i].score < subList[j].score)
-							{
-								rank += 1;
-							}
-						}
-					}
-					subList[i].rank = rank;		
-				}
-				
-				for(int i = 0; i < stSize; i++) 
-				{
-					for(int j = 0; j < subSize; j++) 
-					{
-						if(stList[i].number == subList[j].studentNumber) 
-						{
-							if(subList[j].rank == 1)
-							{
+				for(int i = 0; i < stSize; i++) {
+					for(int j = 0; j < subSize; j++) {
+						if(stList[i].number == subList[j].studentNumber) {
+							if(subList[j].rank == 1) {
 								System.out.print(subList[j].studentNumber + " ");
 								System.out.print(subList[j].subject + " ");
 								System.out.print(subList[j].score + " ");
@@ -166,6 +153,8 @@ public class 클래스배열1_알고리즘_과목1 {
 					}
 				}
 			}
+			
+			
 		}
 
 	}
---
+++ src/G2_클래스배열1_알고리즘/클래스배열1_알고리즘_과목2.java
@@ -0,0 +1,98 @@
+package G2_클래스배열1_알고리즘;
+
+import java.util.Scanner;
+
+
+/*
+class Student{
+	int number;
+	String name;
+}
+
+class Subject{
+	int studentNumber;
+	String subject;
+	int score;
+	int rank;
+}
+ */
+public class 클래스배열1_알고리즘_과목2 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String studentSample = "1001/이만수\n";
+		studentSample += "1002/김철만\n";		
+		studentSample += "1003/오수정\n";
+		
+		String subjectSample = "";
+		subjectSample += "1001/국어/100/0\n";
+		subjectSample += "1001/수학/32/0\n";
+		subjectSample += "1002/국어/23/0\n";
+		subjectSample += "1002/수학/35/0\n";
+		subjectSample += "1002/영어/46/0\n";
+		subjectSample += "1003/수학/60/0";
+		
+		//문제1) 위 샘플문자열들을 각각 해당 클래스배열에 저장후 출력		
+		//문제1) 점수가 59점이하인 과목은 전부 삭제후 다시 문자열로 저장후 출력 
+		
+
+		Student[] stList = null;
+		int stSize = 0;
+		
+		Subject[] subList = null;
+		int subSize = 0;
+		
+		String[] token = studentSample.split("\n");
+		stSize = token.length;
+		stList = new Student[stSize];
+		
+		
+		
+		for(int i = 0; i < stSize; i++)
+		{
+			stList[i] = new Student();
+			
+			String[] token2 = token[i].split("/");
+			
+			stList[i].number = Integer.parseInt(token2[0]);
+			stList[i].name = (token2[1]);
+			
+		}
+		
+		for(int i = 0; i < stSize; i++)
+		{
+			System.out.print(stList[i].number + " ");
+			System.out.println(stList[i].name);
+			
+		}
+		
+		System.out.println("----------------------");
+		String[] to = subjectSample.split("\n");
+		subSize = to.length;
+		subList = new Subject[subSize];
+		
+		for(int i = 0; i < subSize; i++)
+		{
+			subList[i] = new Subject();
+			
+			String[] to2 = to[i].split("/");
+			
+			subList[i].studentNumber = Integer.parseInt(to2[0]);
+			subList[i].subject = to2[1];
+			subList[i].score = Integer.parseInt(to2[2]);
+			subList[i].rank = Integer.parseInt(to2[3]);
+		}
+		
+		for(int i = 0; i < subSize; i++)
+		{
+			System.out.print(subList[i].studentNumber + " ");
+			System.out.print(subList[i].subject + " ");
+			System.out.print(subList[i].score + " ");
+			System.out.println(subList[i].rank);
+		}
+		
+		
+		
+	}
+
+}
